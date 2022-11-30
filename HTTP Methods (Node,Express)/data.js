d7b5c9fe8f84dd2bf8168b8e15373b3f48bb8297
saseivanovski <sase.ivanovski@gmail.com>
2022-11-30 02:36:31 +0100

    methods

---
+++ HTTP Methods (Node,Express)/data.js
@@ -0,0 +1,8 @@
+const people = [
+  { id: 1, name: "saso" },
+  { id: 2, name: "irena" },
+  { id: 3, name: "dragan" },
+  { id: 4, name: "vesna" },
+  { id: 5, name: "marko" },
+];
+module.exports = { people };
